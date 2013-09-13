c69fa2224ca1062436b904a28affb742232e2070
pearman <gabe@Gabes-MacBook-Air.local>
2013-09-13 11:33:46 -0700

    Added Person and People class

---
+++ people.java
@@ -0,0 +1,6 @@
+import Person; // or is it "import person" in lower case?
+
+public class People extends Person
+{
+    
+}
---
+++ person.java
@@ -0,0 +1,18 @@
+//imports will probably be necisarry
+
+public class Person{
+    String name;
+    int age;
+    char gender;
+    String title;
+    
+    //What does he mean initialize the person with a "string of command seperated fields" ?
+    void CreatePerson(String n, int a, char g, String t)
+    {
+        name = n;
+        age = a;
+        gender = g;
+        title = t;
+    }
+}
+
