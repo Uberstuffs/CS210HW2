62bd69ebd386179b2f8239b0027a1da2ec06225a
Uberstuffs <xCodemonkey@Zacks-MacBook-Air.local>
2013-09-14 14:01:08 -0700

    Added all classes/subclasses/skeleton comments to pairem2.java

--- pairem2.java
+++ pairem2.java
@@ -10,26 +10,36 @@ public class pairem2
 
     public class Person
     {
-        String  name;
-        int     age;
-        char    gender;
-        String  Title;
+        String  n;
+        int     a;
+        char    g;
+        String  t;
+        //String major; found in subclass Student    
 
         public void initialize()
         {
-            
+            // parse line of text based on ","
+            //n = first block
+            //a = second block
+            //g = 3rd
+            //t = 4th
         }
     }
    
+
+// Initialize major/department with 5th field from file
+// Can be CS, EE, CE, or Accounting
+//-----
     class Student extends Person
     {
-
+        String major;
     }
 
     class Faculty extends Person
     {
-
+        String department;
     }
+//------
  
     public class People
     {
@@ -49,11 +59,30 @@ public class pairem2
     
     class Students extends People
     {
-        
+        public void oneofeach()
+        {
+            //pair students in grps of 4 with different majors
+            //list those not paired in a group of 4
+            //each column must be 2 spaces wider than longest member
+        }
+
+        public void main(String args[])
+        {
+            //take argv filename and perform "Students" methods on it
+        }
     }
 
     class Instructors extends People
     {
-
+        public void deptrosters()
+        {
+            //alphebetically print department titles and then alphebetically
+            //print names contained therein
+        }
+        
+        public void main(String args[])
+        {
+            //tage argv filename and perform "Facalty" methods on it
+        }
     }
 }
