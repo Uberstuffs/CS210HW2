ed906a2e74b4c7cc1ab0725eb376391957369e01
Uberstuffs <time-seeker@hotmail.com>
2013-09-14 14:12:01 -0700

    person.java (question about initializing class vars answered in comments)

--- person.java
+++ person.java
@@ -7,6 +7,10 @@ public class Person{
     String title;
     
     //What does he mean initialize the person with a "string of command seperated fields" ?
+        //it's actually "comma seperated fields". We initialize it using a line taken from a
+        //file, and parse it based on commas. Each part then becomes it's corresponding piece 
+        //of data. i.e. "James Hamond,28,male,The Top Gear Guy" will be divided into it's 
+        //respective parts. 
     void CreatePerson(String n, int a, char g, String t)
     {
         name = n;
